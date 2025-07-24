abb18ca0206d3f41f8ebcc6bdb452a55d3d0889b
Mario Fabelo <fabeloozcarizmario@gmail.com>
2025-07-24 16:39:15 +0100

    Fixed bug when clicking Create Template

--- src/pages/start.jsx
+++ src/pages/start.jsx
@@ -31,7 +31,7 @@ const StartPage = () => {
           <div className="workout-option-content">
             <div className="workout-icon">💪</div>
             <h3>Start Empty Workout</h3>
-            <p>Begin a custom workout and add exercises as you go</p>
+            <p>Begin a workout and add exercises as you go</p>
           </div>
         </div>
 
